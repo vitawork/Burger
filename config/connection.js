937c2f13d9fc8d08fa465873e91eb28c33c10521
Esteban Morell <estebanmorellb@gmail.com>
2019-09-25 20:41:40 -0500

    files created

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+require("dotenv").config();
+var keys = require("./keys.js");
+
+
+var connection = mysql.createConnection(keys.database_confi);
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers
+    ( burger_name)
+VALUES
+    ("Mushrooms Burger"),
+    ("Tostitos Burger");
+
+INSERT INTO burgers
+    ( burger_name, devoured)
+VALUES
+    ("Kangaroo Burger", true),
+    ("Camel Burger", true);
