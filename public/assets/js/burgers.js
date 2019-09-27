715a45edd06e9511e93471a4db6e4d8bb0d11308
Esteban Morell <estebanmorellb@gmail.com>
2019-09-26 21:14:33 -0500

    switch onclick to submit

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,4 +1,52 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
+function cancelEdit() {
+  $("#newb").css("background-color", "#FFFFFF");
+  $("#newb").attr("placeholder", "Add a new burger");
+}
+
+function insertBurger(event) {
+  event.preventDefault();
+  if (
+    $("#newb")
+      .val()
+      .trim() !== ""
+  ) {
+    $.get(
+      "/api/burger/" +
+        $("#newb")
+          .val()
+          .trim(),
+      function(data) {
+        if (data.length === 0) {
+          $("#newb").css("background-color", "#FFFFFF");
+          $("#newb").attr("placeholder", "Add a new burger");
+          var newB = {
+            burger_name: $("#newb")
+              .val()
+              .trim()
+          };
+          $("#newb").val("");
+          // Send the POST request.
+          $.ajax("/api/burgers", {
+            type: "POST",
+            data: newB
+          }).then(function() {
+            // Reload the page to get the updated list
+            location.reload();
+          });
+        } else {
+          $("#newb").val("");
+          $("#newb").css("background-color", " rgba(194, 125, 125, 0.842)");
+          $("#newb").attr("placeholder", "Allready Exist");
+        }
+      }
+    );
+  } else {
+    $("#newb").css("background-color", " rgba(194, 125, 125, 0.842)");
+    $("#newb").attr("placeholder", "Please type a new burger");
+  }
+}
+
 $(function() {
   $(".devoured").on("click", function(event) {
     event.preventDefault();
@@ -16,46 +64,8 @@ $(function() {
     });
   });
 
-  $("#newBB").on("click", function(event) {
-    event.preventDefault();
-    if (
-      $("#newb")
-        .val()
-        .trim() !== ""
-    ) {
-      $.get(
-        "/api/burger/" +
-          $("#newb")
-            .val()
-            .trim(),
-        function(data) {
-          if (data.length === 0) {
-            $("#newb").css("background-color", "#FFFFFF");
-            $("#newb").attr("placeholder", "Add a new burger");
-            var newB = {
-              burger_name: $("#newb")
-                .val()
-                .trim()
-            };
-            $("#newb").val("");
-            // Send the POST request.
-            $.ajax("/api/burgers", {
-              type: "POST",
-              data: newB
-            }).then(function() {
-              // Reload the page to get the updated list
-              location.reload();
-            });
-          } else {
-            $("#newb").val("");
-            $("#newb").css("background-color", " rgba(194, 125, 125, 0.842)");
-            $("#newb").attr("placeholder", "Allready Exist");
-          }
-        }
-      );
-    } else {
-      $("#newb").css("background-color", " rgba(194, 125, 125, 0.842)");
-      $("#newb").attr("placeholder", "Please type a new burger");
-    }
-  });
+  // $(document).on("click", "#newBB", insertBurger);
+  $(document).on("submit", "#newBForm", insertBurger);
+  $(document).on("blur", "#newBB", cancelEdit);
+  $(document).on("blur", "#newb", cancelEdit);
 });
